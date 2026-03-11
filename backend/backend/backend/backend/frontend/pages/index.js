d3f18aa75f50dbe9e20415d8f0184c817a36fc31
hanahaniha-e <haha1l3718@gmail.com>
2026-03-11 22:21:16 +0800

    Update index.js

--- backend/backend/backend/backend/frontend/pages/index.js
+++ backend/backend/backend/backend/frontend/pages/index.js
@@ -1,1 +1,43 @@
+import { useState } from "react"
 
+export default function Home(){
+
+ const [url,setUrl]=useState("")
+
+ const submit=async()=>{
+
+  await fetch("http://localhost:8000/add",{
+
+   method:"POST",
+
+   headers:{
+    "Content-Type":"application/json"
+   },
+
+   body:JSON.stringify({url})
+
+  })
+
+  alert("開始監控")
+ }
+
+ return(
+
+  <div style={{padding:40}}>
+
+   <h1>Restock Alert</h1>
+
+   <input
+    value={url}
+    onChange={(e)=>setUrl(e.target.value)}
+    placeholder="商品網址"
+   />
+
+   <button onClick={submit}>
+    開始監控
+   </button>
+
+  </div>
+
+ )
+}
